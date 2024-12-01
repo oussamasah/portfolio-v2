04eec0d119687a30b2ad1a925c3fde9f9eeb9734
Sahraoui Oussama <92527684+oussamasah@users.noreply.github.com>
2024-12-01 14:20:06 +0100

    Create envirement.ts

---
+++ src/envirement.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://portfoliobackend-j7qmn9v66-sahraoui-oussamas-projects.vercel.app/',
+    captchaSiteKey: "6Lce_o0qAAAAAOa9jfpi7YUzYTxVOFVPJ5tQui4V"
+};
